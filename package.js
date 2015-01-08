ea5f1db0eae25d22d965d3ee067a8fbfb8bb39d7
Douglas Riches <doug@obtuserobot.com>
2015-01-08 19:34:54 +0000

    removed extra space at end of git url in package.js

--- package.js
+++ package.js
@@ -5,13 +5,13 @@ Npm.depends({
 Package.describe({
   name: 'dougr:imagemagik-stream',
   summary: 'adding stream support for imagemagik',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  version: '1.0.1',
+  git: 'https://github.com/driches/dougr-imagemagik-stream'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
-  api.export('im');
+  api.export('im'); //export our exposed NPM functionality
   api.addFiles(['dougr:imagemagik-stream.js'],'server');
 });
 
